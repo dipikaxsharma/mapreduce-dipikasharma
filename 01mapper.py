0fcc308924ef5601ddca84f4df183dbcbc665df8
dipikaxsharma <s530527@nwmissouri.edu>
2022-02-10 10:13:08 -0600

    Final work

--- 01mapper.py
+++ 01mapper.py
@@ -3,8 +3,8 @@ import sys
 # iterate through each line provided via standard input
 for line in sys.stdin:
   datalist = line.strip().split(",")
-  if (len(datalist) == 6) : 
-    name,diet,prep_time,cook_time,flavor_profile,course = datalist
+  if (len(datalist) == 8) : 
+    name,diet,prep_time,cook_time,flavor_profile,course,state,region = datalist
 
     # print intermediate key-value pairs to standard output
-    print(diet,"\t",1)
+    print(name,"\t",region)
--- 02reducer.py
+++ 02reducer.py
@@ -1,24 +1,27 @@
+# Dipika 2 - Reducer using standard input and output
+# Easy to test locally in the terminal
+
 import sys
 
 thisKey = ""
-thisValue = 0.0
+thisValue = ""
 
 for line in sys.stdin:
   datalist = line.strip().split('\t')
   if (len(datalist) == 2) : 
-    foodName, count = datalist
+    foodtype, region = datalist
 
-    if foodName != thisKey:   # we've moved to another key
+    if foodtype != thisKey:   # we've moved to another key
       if thisKey:
         # output the previous key-summaryvalue result
         print(thisKey,'\t',thisValue)
 
       # start over for each new key
-      thisKey = foodName 
-      thisValue = 0.0
+      thisKey = foodtype 
+      thisValue = region
   
     # apply the aggregation function
-    thisValue += float(count)
+   # thisValue += float(count)
 
 # output the final key-summaryvalue result outside the loop
 print(thisKey,'\t',thisValue)
